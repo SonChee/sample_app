5cb1a3f8d9706f67d35153d22651ecc2254eb2d1
Son Che <che.dinh.son@framgia.com>
2014-01-08 14:07:29 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)   #check strong parameter
+  	if @user.save  # if check parameter is ok, user.save
+      flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user #if user.save is ok, redirect to new page.
+  	else
+  		render 'new'
+  	end
+  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
